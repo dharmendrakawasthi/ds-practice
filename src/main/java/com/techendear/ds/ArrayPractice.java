d1f78b97fee7280d6cb9c1b0ed707b316638f6d0
Dharmendra Awasthi <dharmendrakawasthi@gmail.com>
2020-03-01 11:05:11 +0530

    Reinitialize

---
+++ src/main/java/com/techendear/ds/ArrayPractice.java
@@ -0,0 +1,5 @@
+package com.techendear.ds;
+
+public class ArrayPractice {
+
+}
--- src/main/java/com/techendear/linkedlist/MainClass.java
+++
@@ -1,8 +0,0 @@
-package com.techendear.linkedlist;
-
-public class MainClass {
-
-	public static void main(String[] args) {
-		
-	}
-}
